a9423ab6876fbd1fc8d0d7d9f68900378c504b08
abdurrahman <32853027+asaah18@users.noreply.github.com>
2020-03-19 22:40:33 +0300

    add breadth first search
    additional fixes:
    - print a statment if no route to be found
    - rename "visited_cities" into visited_places

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from data_structure import Graph, Stack
+from data_structure import Graph, Stack, Queue
 
 
 def get_user_choice(choices) -> int:
@@ -222,7 +222,7 @@ class MaccaMap:
         """set the starting place as a string"""
         cities = self.graph.get_all_nodes()
         print("Choose a place number to start with:" + "\n")
-        # self.start_city = cities[get_user_choice(cities)]
+        # start_place = cities[get_user_choice(cities)]
         start_place = cities[0]
         print("you will start at", start_place, "place")
         return start_place
@@ -231,7 +231,7 @@ class MaccaMap:
         """set the starting place as a string"""
         cities = self.graph.get_all_nodes()
         print("Choose a place number as a target:" + "\n")
-        # self.start_city = cities[get_user_choice(cities)]
+        # start_place = cities[get_user_choice(cities)]
         target_place = cities[1]
         print("your target place is", target_place, "place")
         return target_place
@@ -242,23 +242,26 @@ class FindRoute:
 
     def __init__(self):
         self.stack = Stack()
+        self.queue = Queue()
         self.meccaMap = MaccaMap()
         self.start_place = self.meccaMap.set_starting_place()
         self.target_place = self.meccaMap.set_target_place()
         self.depth_first_search()
+        print("="*25, "\n", "end of DFS", "="*25)
+        self.breadth_first_search()
 
     def depth_first_search(self):
         """travel form start_place to target_place from left to right while logging the traveled cities"""
-        visited_cities = []
+        visited_places = []
         print("----------Depth First Search Traverse-------------")
         self.stack.unique_push(self.start_place)
 
         while not self.stack.is_empty():
             current_place = self.stack.pop()
-            visited_cities.append(current_place)
+            visited_places.append(current_place)
             print("I am at", current_place, "place")
             self.stack.display()
-            print("Visited cities are:", visited_cities)
+            print("Visited cities are:", visited_places)
             if current_place is self.target_place:
                 print("I have reached the target place")
                 break
@@ -274,12 +277,52 @@ class FindRoute:
 
                 for node in connected_nodes:
                     # push to stack if not visited and not in stack
-                    if node['name'] not in visited_cities:
+                    if node['name'] not in visited_places:
                         self.stack.unique_push(node['name'])
                         # print(node, "has been added to stack")
 
                 self.stack.display()
             print("-----------------------------------------")
+        else:
+            # executed if target not found(break statement not executed)
+            print("I wasn't able to find a route")
+
+    def breadth_first_search(self):
+        """travel form start_place to target_place from left to right breadth first while logging the traveled cities"""
+        visited_places = []
+        print("----------Breadth First Search Traverse-------------")
+        self.queue.unique_enqueue(self.start_place)
+
+        while not self.queue.is_empty():
+            current_place = self.queue.dequeue()
+            visited_places.append(current_place)
+            print("I am at", current_place, "place")
+            self.queue.display()
+            print("Visited places are:", visited_places)
+            if current_place is self.target_place:
+                print("I have reached the target place")
+                break
+            else:
+                connected_nodes = self.meccaMap.graph.get_children_of_node(current_place)
+
+                print("The connected nodes are:")
+                print()
+                for node in connected_nodes:
+                    for key, value in node.items():
+                        print(key, ': ', value)
+                    print()
+
+                for node in connected_nodes:
+                    # push to stack if not visited and not in stack
+                    if node['name'] not in visited_places:
+                        self.queue.unique_enqueue(node['name'])
+                        # print(node, "has been added to stack")
+
+                self.queue.display()
+            print("-----------------------------------------")
+        else:
+            # executed if target not found(break statement not executed)
+            print("I wasn't able to find a route")
 
 
 FindRoute()
